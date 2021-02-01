46b46f8306c38068a37ae9eea0ccb45b2fe170ca
johnteruel <78330635+johnteruel@users.noreply.github.com>
2021-02-02 00:24:29 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE htmlL>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=divice-width, initial-scale=1.0">
+ <title> Document</title>
+ </head>
+ <body>
+ <p>Welcome to my page [Write your full name here] </p>
+ </body>
+ </html>
